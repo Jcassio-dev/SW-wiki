de005159328bd6b42aa5ec2903a811de46368217
José Cássio <josecassio2013@gmail.br>
2023-07-19 12:00:55 -0300

    add tailwin config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      'mobile': {'min': '428px'},
+
+      'tablet': {'min': '768px'},
+
+      'desktop': {'min': '1024px'},
+
+    },
     extend: {},
   },
   plugins: [],
