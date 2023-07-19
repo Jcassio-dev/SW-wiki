bbf00379cb9d6b62073c048f04e8df2d83535143
José Cássio <josecassio2013@gmail.br>
2023-07-19 13:26:27 -0300

    add filter with search

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,19 +5,25 @@ import { Header } from "../../components/Header";
 
 export function Home() {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     async function fetchPeople(){
         await swapi.get(`/people`).then(({data}) => {setData(data.results), console.log(data)})
     }
     fetchPeople();
-},  []);
+  },  []);
+
+  const filteredPeople = search.length > 0 
+  ? data.filter(data => data.name.includes(search)) 
+  : data
 
  return (
     <>
       <Header/>
+      <input type="text" onChange={e => setSearch(e.target.value)}  value={search}className="text-black"/>
     {
-      data.map((people, index) => <h1 key={index} className="">{people.name}</h1>)
+      filteredPeople.map((people, index) => <h1 key={index} className="">{people.name}</h1>)
     }
     </>
   )
