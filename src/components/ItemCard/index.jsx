aa0b5e33943beaeccee7d0ef13a48de22a6cd78d
José Cássio <josecassio2013@gmail.com>
2023-07-21 13:02:48 -0300

    add link function in card

--- src/components/ItemCard/index.jsx
+++ src/components/ItemCard/index.jsx
@@ -1,10 +1,23 @@
-export function ItemCard({title, section,info1, info2, icon: Icon}){
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { useAuth } from "../../hooks/auth";
+
+export function ItemCard({title, section,info1, info2, link, icon: Icon}){
+    const navigate = useNavigate();
+    const {user} = useAuth();
+
+    function alertUser(){
+        toast.warn("Você precisa estar logado para acessar os detalhes!")
+        navigate('/')
+    }
+
     return(
-        <div className="w-64 p-5 flex flex-col border rounded-md cursor-pointer transition duration-500 mx-auto hover:bg-yellow-300 hover:text-black hover:-translate-y-2">
+        <button onClick={() => {user ? navigate(link) : alertUser()}}className="w-64 p-5 flex flex-col border rounded-md cursor-pointer transition duration-500 mx-auto hover:bg-yellow-300 hover:text-black hover:-translate-y-2">
             <h2 className="text-xl font-bold border-b flex items-center gap-1">{Icon && <Icon/>}{title}</h2>
             <h3 className="text-lg font-medium">{section}</h3>
             <p>{info1}</p>
             <p>{info2}</p>
-        </div>
+        </button>
     )
 }
