ff42504e33b3a33807c19154d25d61e704dfb546
José Cássio <josecassio2013@gmail.br>
2023-07-21 00:27:48 -0300

    add text align center

--- src/components/Fetching/index.jsx
+++ src/components/Fetching/index.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/emblem.svg'
 export function Fetching(){
     return(
         <div className='w-9/12 mx-auto my-8 flex flex-col items-center justify-center'>
-            <h1 className='text-3xl font-bold text-yellow-300'>Os dados estão vindo de uma galáxia muito, muito distante...</h1>
+            <h1 className='text-3xl font-bold text-yellow-300 text-center'>Os dados estão vindo de uma galáxia muito, muito distante...</h1>
             <img src={logo} alt="Logo girando" className='animate-spin w-20 h-20'/>
       </div>  
     )
