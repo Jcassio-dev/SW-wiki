23a3383cfb0cbc55bb23f02aa7f2c36b4e4fdeac
José Cássio <josecassio2013@gmail.com>
2023-07-21 10:12:16 -0300

    set characters page = home

--- src/routes/App.routes.jsx
+++ src/routes/App.routes.jsx
@@ -1,15 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
 
-import { Home } from '../pages/Home';
 import { Starships } from '../pages/Starships';
-import { Characters } from '../pages/Characters';
+import { Home } from '../pages/Home';
 import { Planets } from '../pages/Planets';
 
 export function AppRoutes(){
     return(
         <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/characters" element={<Characters/>}/>
             <Route path="/starships" element={<Starships/>}/>
             <Route path="/planets" element={<Planets/>}/>
         </Routes>
