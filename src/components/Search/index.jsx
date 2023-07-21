582f95d1653fdf6f8c1d6c5d6b556a88e360985a
José Cássio <josecassio2013@gmail.br>
2023-07-21 01:33:27 -0300

    change width

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,9 +2,9 @@ import { AiOutlineSearch } from 'react-icons/ai'
 
 export function Search({...rest}){
     return(
-    <div className="w-9/12 flex items-center text-black bg-white px-2 py-2 rounded-md gap-1 mx-auto my-4 animate-left">
+    <div className="desktop:w-80 mobile:w-40 flex items-center text-yellow-300 bg-black px-2 py-2 rounded-md gap-1 border border-yellow-300">
         <AiOutlineSearch className='text-lg'/>
-        <input {...rest} type="text" className="text-black w-full rounded-sm bg-white outline-none border-0"/>
+        <input {...rest} type="text" className="text-yellow-300 w-full rounded-sm bg-black outline-none border-0 placeholder:text-yellow-700"/>
     </div>
     )
 }
