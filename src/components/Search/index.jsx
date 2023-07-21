5dd6369774a2dfa93a8adb90c8f194729ea75944
José Cássio <josecassio2013@gmail.com>
2023-07-21 12:27:14 -0300

    change placeholder color

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -4,7 +4,7 @@ export function Search({...rest}){
     return(
     <div className="desktop:w-80 mobile:w-40 flex items-center text-yellow-300 bg-black px-2 py-2 rounded-md gap-1 border border-yellow-300">
         <AiOutlineSearch className='text-lg'/>
-        <input {...rest} type="text" className="text-yellow-300 w-full rounded-sm bg-black outline-none border-0 placeholder:text-yellow-700"/>
+        <input {...rest} type="text" className="text-yellow-300 w-full rounded-sm bg-black outline-none border-0 placeholder:text-white"/>
     </div>
     )
 }
