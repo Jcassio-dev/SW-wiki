e0b54a332ef6824a856e44dd9ffa1f4c91600068
José Cássio <josecassio2013@gmail.com>
2023-07-21 12:49:52 -0300

    add register system

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,17 +1,51 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 import emblem from '../../assets/emblem.svg'
 
 import { Header } from "../../components/Header";
 import { InputWrapper } from "../../components/InputWrapper";
+
 import { api } from "../../services/api";
+import { useAuth } from "../../hooks/auth";
 
 
-export function Login() {
+
+export function Register() {
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [verify, setVerify] = useState(false)
+
+    const { signIn } = useAuth()
+
+    const navigate = useNavigate()
+
+    function handleSignIn(){
+        signIn({email, password})
+    }
+    async function handleSignUp(){
+        if(!name || !email || !password){
+         toast.warn("Preencha todos os campos!")
+         return
+        }
+
+        await api.post("/users", {name, email, password})
+        .then(() => {
+            toast.success("Usuário cadastrado com sucesso!")
+            handleSignIn({email, password})
+            navigate('/')
+        }).catch(error => {
+            if(error.response){
+                toast.error(error.response.data.message)
+            }else{
+                toast.error("Não foi possível cadastrar");
+            }
+        });
+        
+    }
+
     useEffect(()=>{
         async function verifyAPI(){
             await api.get('/users').then(({data}) => {setVerify(true), console.log(data)})
@@ -23,17 +57,19 @@ export function Login() {
     <>
       <Header page={0}/>
         <main className="mt-32 desktop:px-40 tablet:px-30 mobile:px-14 w-full">
-            <h1 className='desktop:text-3xl mobile:text-xl font-bold text-center text-yellow-300'>Una-se ao lado registrado da força!</h1>
-            <div className="flex items-center justify-center my-5">
-                <form className="flex flex-col mobile:gap-5 desktop:gap-7 mobile:w-64 desktop:w-96 p-5 border border-yellow-300 rounded-md">
+            <h1 className='desktop:text-3xl mobile:text-xl font-bold text-center text-yellow-300 animate-top'>Una-se ao lado registrado da força!</h1>
+            <div className="flex items-center justify-center my-5 animate-bottom">
+                <div className="flex flex-col mobile:gap-5 desktop:gap-7 mobile:w-64 desktop:w-96 p-5 border border-yellow-300 rounded-md">
                     <img src={emblem} alt="Emblema do império galático" className='w-12 h-12 mx-auto'/>
+                    <InputWrapper type="text" placeholder="Digite aqui seu nome" label={"Nome"} value={name} onChange={e => setName(e.target.value)}/>
                     <InputWrapper type="email" placeholder="Digite aqui seu email" label={"E-mail"} value={email} onChange={e => setEmail(e.target.value)}/>
                     <InputWrapper type="password" placeholder="Digite aqui sua senha" label={"Senha"} value={password} onChange={e => setPassword(e.target.value)}/>
-                    <button className="p-2 bg-yellow-300 text-black font-bold rounded-md cursor-pointer transition duration-300 hover:scale-105 disabled:brightness-75 disabled:cursor-default disabled:scale-100" disabled={!verify}>{verify? 'Entrar' : 'conectando...'}</button>
-                    <span className="text-center">Não possui uma conta? <br/> <Link to="/register" className="text-yellow-300 underline">Registre-se aqui</Link></span>
-                </form>
+                    <button onClick={handleSignUp} className="p-2 bg-yellow-300 text-black font-bold rounded-md cursor-pointer transition duration-300 hover:scale-105 disabled:brightness-75 disabled:cursor-default disabled:scale-100" disabled={!verify}>{verify? 'Registrar' : 'conectando...'}</button>
+                    <span className="text-center">Já possui uma conta? <br/> <Link to="/" className="text-yellow-300 underline">Acesse aqui</Link></span>
+                </div>
             </div>
         </main>
     </>
   )
 }
+
--- src/routes/App.routes.jsx
+++ src/routes/App.routes.jsx
@@ -4,11 +4,13 @@ import { Starships } from '../pages/Starships';
 import { Home } from '../pages/Home';
 import { Planets } from '../pages/Planets';
 import { Login } from '../pages/Login';
+import { Register } from '../pages/Register';
 
 export function AppRoutes(){
     return(
         <Routes>
             <Route path="/" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
             <Route path="/characters" element={<Home/>}/>
             <Route path="/starships" element={<Starships/>}/>
             <Route path="/planets" element={<Planets/>}/>
