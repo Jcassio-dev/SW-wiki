d51d371c7e12861c6232b2742b72176ca5f6f96d
José Cássio <josecassio2013@gmail.com>
2023-07-21 11:48:54 -0300

    add more animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,14 +16,24 @@ export default {
           '0%': { transform: 'translateX(-100px)', opacity: 0 },
           '100%': { transform: 'translateX(0)', opacity: 1 },
         },
+        right: {
+          '0%': { transform: 'translateX(100px)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
+        },
         bottom:{
           '0%': { transform: 'translateY(100px)', opacity: 0 },
           '100%': { transform: 'translateY(0)', opacity: 1 },
-        }
+        },
+        top:{
+          '0%': { transform: 'translateY(-100px)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
       },
       animation: {
         left: 'left 0.3s ease-in-out',
-        bottom: 'bottom 0.3s ease-in-out'
+        bottom: 'bottom 0.3s ease-in-out',
+        top: 'top 0.3s ease-in-out',
+        right: 'right 0.3s ease-in-out',
       }
     },
   },
