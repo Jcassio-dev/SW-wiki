3a1b167b6e13e5d4d9b1b8eaff87d7aee3f05c57
José Cássio <josecassio2013@gmail.br>
2023-07-21 00:29:26 -0300

    add icon for cards

--- src/components/ItemCard/index.jsx
+++ src/components/ItemCard/index.jsx
@@ -1,7 +1,7 @@
-export function ItemCard({title, info1, info2}){
+export function ItemCard({title, info1, info2, icon: Icon}){
     return(
         <div className="w-64 p-5 flex flex-col border rounded-md cursor-pointer transition duration-500 mx-auto hover:bg-yellow-300 hover:text-black hover:-translate-y-2">
-            <h2 className="text-2xl font-medium border-b">{title}</h2>
+            <h2 className="text-2xl font-medium border-b">{Icon}{title}</h2>
             <p>{info1}</p>
             <p>{info2}</p>
         </div>
